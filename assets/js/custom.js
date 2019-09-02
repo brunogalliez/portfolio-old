0c0772ec5806850c861ba9b3748e9af851c9c1d8
Bruno Galliez <brunogalliez@gmail.com>
2019-09-02 16:37:30 -0300

    change mailto to copy-email

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -10,3 +10,11 @@
 
 		
 })(jQuery);
+
+function copyToClipboard(element) {
+	var $temp = $("<input>");
+	$("body").append($temp);
+	$temp.val($(element).text()).select();
+	document.execCommand("copy");
+	$temp.remove();
+  }
